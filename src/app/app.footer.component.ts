8e4d7a9ecb03254b09ed38693d6b1b2685075c9b
Puntonatural <116592883+Puntonatural@users.noreply.github.com>
2023-03-11 08:23:57 -0500

    first commit

---
+++ src/app/app.footer.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-footer',
+    template: `
+        <div class="layout-footer">
+			<div >
+				<img src="assets/layout/images/Logo_Fondo_Blanco_40h.png" alt="mirage-layout" />
+				<div class="text">
+					
+					
+				</div>
+			</div>
+			<div class="icons">
+				<div class="icon icon-hastag">
+					<i class="pi pi-home"></i>
+				</div>
+				<div class="icon icon-twitter">
+					<i class="pi pi-globe"></i>
+				</div>
+				<div class="icon icon-prime">
+					<i class="pi pi-bookmark"></i>
+				</div>
+			</div>
+        </div>
+    `
+})
+export class AppFooterComponent {
+
+}
