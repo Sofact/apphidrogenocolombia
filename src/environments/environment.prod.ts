6d178192afb003d5c734651a1af0fe2f9ac81b71
miguelangelrd <miguelangel.rd@hotmail.com>
2023-03-23 01:48:38 -0500

    cambio de enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  HOST_BACKEND: '127.0.0.1',
-  URL_SERVICIOS: "HTTP://127.0.0.1:8000/api",
-  URL_BACKEND: "HTTP://127.0.0.1:8000",
-  URL_FRONTEND: "HTTP://127.0.0.1:4200"
+  HOST_BACKEND: '/',
+  URL_SERVICIOS: "/api",
+  URL_BACKEND: "/api",
+  URL_FRONTEND: "/"
 };
